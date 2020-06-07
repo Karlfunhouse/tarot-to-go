33860dfe672d1fda0787f6098b8c1f31095d0d1e
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-06-07 16:11:47 -0600

    :crystal_ball: Updating header layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,14 @@ const Header = () => {
     return (
         <header>
             <div>
-                <img src="/images/crystal-ball.png" className="crystal-ball"/>
+                <img src="/images/crystal-ball.png" className="icon"/>
             </div>
             <div className="header-text">
                 <h2>TAROT TO GO</h2> 
                 <h4>For Mystics on the Move</h4>
+            </div>
+            <div>
+                <img src="/images/magic-mist.png" className="icon"/>
             </div>
         </header>
     )
